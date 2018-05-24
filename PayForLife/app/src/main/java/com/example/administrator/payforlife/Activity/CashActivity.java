19ec79a46b05f7a84e1aabb31e76115f94cee9a9
zhaoqi7 <446110229@qq.com>
2018-05-24 15:43:59 +0800

    个人财产页面的两个子页面

--- PayForLife/app/src/main/java/com/example/administrator/payforlife/Activity/CashActivity.java
+++ PayForLife/app/src/main/java/com/example/administrator/payforlife/Activity/CashActivity.java
@@ -1,7 +1,9 @@
 package com.example.administrator.payforlife.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.administrator.payforlife.R;
 
@@ -11,5 +13,10 @@ public class CashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cash);
+    }
+    public void goback(View view){
+        Intent intent = new Intent();
+        intent.setClass(CashActivity.this,Personal_propertyActivity.class);
+        startActivity(intent);
     }
 }
