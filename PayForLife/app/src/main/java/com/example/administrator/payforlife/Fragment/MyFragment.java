5cad516a6ecc52834859135a783b50c84a07a174
wanghongwei <1172768786@qq.com>
2018-05-24 14:47:31 +0800

    首页和个人信息页面的编写

--- PayForLife/app/src/main/java/com/example/administrator/payforlife/Fragment/MyFragment.java
+++ PayForLife/app/src/main/java/com/example/administrator/payforlife/Fragment/MyFragment.java
@@ -8,8 +8,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.administrator.payforlife.Activity.CashActivity;
+import com.example.administrator.payforlife.Activity.Personal_propertyActivity;
+import com.example.administrator.payforlife.Activity.Setting_Activity;
 import com.example.administrator.payforlife.R;
 
 /**
@@ -19,15 +23,29 @@ import com.example.administrator.payforlife.R;
 public class MyFragment extends Fragment {
     private Context mContext;
     private TextView tvInstall;
+    private Button btnBalance;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.my_layout,container,false);
         tvInstall=view.findViewById(R.id.tv_install);
+        btnBalance=view.findViewById(R.id.btn_balance);
+
         tvInstall.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent=new Intent();
+                intent.setClass(mContext, Setting_Activity.class);
+                startActivity(intent);
+            }
+        });
 
+        btnBalance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent();
+                intent.setClass(mContext, Personal_propertyActivity.class);
+                startActivity(intent);
             }
         });
         return view;
--- PayForLife/app/src/main/java/com/example/administrator/payforlife/Fragment/ShouyeFragment.java
+++ PayForLife/app/src/main/java/com/example/administrator/payforlife/Fragment/ShouyeFragment.java
@@ -1,13 +1,16 @@
 package com.example.administrator.payforlife.Fragment;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.administrator.payforlife.Activity.Personal_propertyActivity;
 import com.example.administrator.payforlife.R;
 
 /**
@@ -16,10 +19,20 @@ import com.example.administrator.payforlife.R;
 
 public class ShouyeFragment extends Fragment {
     private Context mContext;
+    private Button btnBalance;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.shouye_layout,container,false);
+        btnBalance=view.findViewById(R.id.btn_balance);
+        btnBalance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent();
+                intent.setClass(mContext, Personal_propertyActivity.class);
+                startActivity(intent);
+            }
+        });
         return view;
     }
 
